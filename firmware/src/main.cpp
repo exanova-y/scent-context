77c4f11a70dbcc3d58fc2826f0d9c44cbae955ae
eigenlucy <heartlheart_dit@protonmail.com>
2025-04-20 01:53:49 -0700

    added firmware (configured for ESP32 rn for testing)

---
+++ firmware/src/main.cpp
@@ -0,0 +1,21 @@
+#include "Arduino.h"
+#include "BME688.h"
+
+BME688 sensor;
+
+void setup() {
+    Serial.begin(9600);
+    if (sensor.begin()) {
+        Serial.println("BME688 Initialized Successfully!");
+    } else {
+        Serial.println("Failed to initialize BME688!");
+    }
+}
+
+void loop() {
+    Serial.print("Gas Resistance: ");
+    Serial.print(sensor.readGas(0));
+    Serial.println(" Ω");
+    
+    delay(5000);
+}
